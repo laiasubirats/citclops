0ace22986f96e6b0efbb0ef8ad0b25e03cfdace7
lsubirats@bdigital.org <lsubirats@bdigital.org@72c96493-abc8-aad3-1984-a4d6509c7d01>
2014-04-08 07:53:43 +0000

    java

---
+++ branches/JerseyFileUpload.java
@@ -0,0 +1,67 @@
+package Upload;
+  
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
+
+@Path("/files")
+public class JerseyFileUpload {
+
+	private static final String SERVER_UPLOAD_LOCATION_FOLDER = "C:\\";
+	//private static final String SERVER_UPLOAD_LOCATION_FOLDER = "/home/citclops/data";
+
+	/**
+	 * Upload a File
+	 */
+
+	@POST
+	@Path("/upload")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response uploadFile(
+			@FormDataParam("file") InputStream fileInputStream,
+			@FormDataParam("file") FormDataContentDisposition contentDispositionHeader) {
+
+		String filePath = SERVER_UPLOAD_LOCATION_FOLDER	+ contentDispositionHeader.getFileName();
+
+		// save the file to the server
+		saveFile(fileInputStream, filePath);
+
+		//String output = "File saved to server location : " + filePath;
+		String output = "File saved to Citclops server";
+
+		return Response.status(200).entity(output).build();
+
+	}
+
+	// save uploaded file to a defined location on the server
+	private void saveFile(InputStream uploadedInputStream,
+			String serverLocation) {
+
+		try {
+			OutputStream outpuStream = new FileOutputStream(new File(serverLocation));
+			int read = 0;
+			byte[] bytes = new byte[1024];
+
+			outpuStream = new FileOutputStream(new File(serverLocation));
+			while ((read = uploadedInputStream.read(bytes)) != -1) {
+				outpuStream.write(bytes, 0, read);
+			}
+			outpuStream.flush();
+			outpuStream.close();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+}
